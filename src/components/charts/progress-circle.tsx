7194157671cc6c478338c516690e060cc765a4e5
Andreas Huber <j7ha0873@unibw.de>
2020-09-30 20:38:44 +0200

    add progress bar as solidgauge

---
+++ src/components/charts/progress-circle.tsx
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import Highcharts from "highcharts";
+import highchartsMore from "highcharts/highcharts-more.js";
+import solidGauge from "highcharts/modules/solid-gauge.js";
+import HighchartsReact from "highcharts-react-official";
+
+if (typeof Highcharts === "object") {
+  highchartsMore(Highcharts);
+  solidGauge(Highcharts);
+}
+
+type GaugeProps = {
+  title: string;
+  subtitle: string;
+  percentage: number;
+};
+
+const ProgressCircle = ({ title, subtitle, percentage }: GaugeProps) => {
+  const [chartOptions, setChartOptions] = useState<Highcharts.Options>({
+    chart: {
+      type: "solidgauge",
+    },
+
+    title: {
+      text: title,
+    },
+
+    tooltip: {
+      enabled: false,
+    },
+
+    yAxis: {
+      min: 0,
+      max: 100,
+      lineWidth: 0,
+      tickPositions: [],
+    },
+
+    plotOptions: {
+      solidgauge: {
+        borderWidth: 0,
+        dataLabels: {
+          enabled: true,
+          y: -50,
+          borderWidth: 0,
+          backgroundColor: "none",
+          useHTML: true,
+          shadow: false,
+          style: {
+            fontSize: "16px",
+          },
+          formatter: function () {
+            return (
+              '<span style="font-size:2em;color:' +
+              Highcharts.getOptions().colors[0] +
+              ';font-weight:bold;">' +
+              percentage +
+              "%" +
+              '</span><br/><span style="font-size:1em;color:' +
+              Highcharts.getOptions().colors[1] +
+              ';font-weight:bold;">' +
+              subtitle +
+              "</span>"
+            );
+          },
+        },
+      },
+    },
+
+    series: [
+      {
+        name: title,
+        type: "solidgauge",
+        borderColor: Highcharts.getOptions().colors[1],
+        data: [
+          {
+            color: Highcharts.getOptions().colors[0],
+            radius: "110%",
+            innerRadius: "90%",
+            y: percentage,
+          },
+        ],
+      },
+    ],
+  });
+
+  return (
+    <div>
+      <HighchartsReact highcharts={Highcharts} options={chartOptions} />
+    </div>
+  );
+};
+
+export default ProgressCircle;
--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -1,49 +1,59 @@
-import React from 'react';
-import { Grid } from '@material-ui/core';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import useStyles from './styles';
-import GoodsChart from '../charts/goods';
-import ProductionChart from '../charts/production';
-import DeliveryChart from '../charts/delivery';
+import React from "react";
+import { Grid } from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import useStyles from "./styles";
+import GoodsChart from "../charts/goods";
+import ProductionChart from "../charts/production";
+import DeliveryChart from "../charts/delivery";
+import ProgressCircle from "../charts/progress-circle";
 
 const Dashboard = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <Grid container className={classes.dashboardContainer} spacing={2}>
-            <Grid item xs={4}>
-                <Card variant="outlined">
-                    <CardContent>
-                    Wareneingänge
-                    <div className={classes.goods}>
-                        <GoodsChart />
-                    </div>
-                    </CardContent>
-                </Card>
-            </Grid>
-            <Grid item xs={4}>
-                <Card variant="outlined">
-                    <CardContent>
-                    Produktionsprozesse
-                    <div className={classes.production}>
-                        <ProductionChart />
-                    </div>
-                    </CardContent>
-                </Card>
-            </Grid>
-            <Grid item xs={4}>
-                <Card variant="outlined"> 
-                    <CardContent>
-                    Produktionsprozesse
-                    <div className={classes.delivery}>
-                        <DeliveryChart />
-                    </div>
-                    </CardContent>
-                </Card>
-            </Grid>
+  return (
+    <div>
+      <Grid container className={classes.dashboardContainer} spacing={2}>
+        <Grid item xs={4}>
+          <Card variant="outlined">
+            <CardContent>
+              Wareneingänge
+              <div className={classes.goods}>
+                <GoodsChart />
+              </div>
+            </CardContent>
+          </Card>
         </Grid>
-    )
+        <Grid item xs={4}>
+          <Card variant="outlined">
+            <CardContent>
+              Produktionsprozesse
+              <div className={classes.production}>
+                <ProductionChart />
+              </div>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={4}>
+          <Card variant="outlined">
+            <CardContent>
+              Produktionsprozesse
+              <div className={classes.delivery}>
+                <DeliveryChart />
+              </div>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+      <div className={classes.circle}>
+        <ProgressCircle
+          title="Wareneingänge"
+          subtitle="5 Proben"
+          percentage={60}
+        ></ProgressCircle>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
