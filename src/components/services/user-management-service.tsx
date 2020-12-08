1e3b72a54716e496886a77c995dcb2b6c4ad66f2
Andreas Huber <j7ha0873@unibw.de>
2020-12-08 20:09:50 +0100

    add user management service

---
+++ src/components/services/user-management-service.tsx
@@ -0,0 +1,114 @@
+import instance from "../../axios";
+import authHeader from "./auth-header";
+
+class UserManagementService {
+  // CREATE, UPDATE, DELETE
+  // Users:
+  createUser(username: string, password: string) {
+    return instance.post(
+      "/submit?function=createUser",
+      { username, password },
+      {
+        headers: authHeader(),
+      }
+    );
+  }
+
+  deleteUser(username: string) {
+    return instance.post(
+      "/submit?function=deleteUser",
+      { username },
+      {
+        headers: authHeader(),
+      }
+    );
+  }
+
+  linkUserToWhitelist(whitelist: string, username: string) {
+    return instance.post(
+      "/submit?function=linkUserToWhitelist",
+      { whitelist, username },
+      {
+        headers: authHeader(),
+      }
+    );
+  }
+
+  unlinkUserFromWhitelist(whitelist: string, username: string) {
+    return instance.post(
+      "/submit?function=unlinkUserFromWhitelist",
+      { whitelist, username },
+      {
+        headers: authHeader(),
+      }
+    );
+  }
+
+  // Whitelists
+  createWhitelist(whitelist: string) {
+    return instance.post(
+      "/submit?function=createWhitelist",
+      { whitelist },
+      {
+        headers: authHeader(),
+      }
+    );
+  }
+  deleteWhitelist(whitelist: string) {
+    return instance.post(
+      "/submit?function=deleteWhitelist",
+      { whitelist },
+      {
+        headers: authHeader(),
+      }
+    );
+  }
+
+  // function is a keyword in Typescript, so func is used
+  linkFunctionToWhitelist(whitelist: string, func: string) {
+    return instance.post(
+      "/submit?function=linkFunctionToWhitelist",
+      { whitelist, function: func },
+      {
+        headers: authHeader(),
+      }
+    );
+  }
+
+  unlinkFunctionFromWhitelist(whitelist: string, func: string) {
+    return instance.post(
+      "/submit?function=unlinkFunctionFromWhitelist",
+      { whitelist, function: func },
+      {
+        headers: authHeader(),
+      }
+    );
+  }
+
+  // READ
+  getAllUsers() {
+    return instance.get("/get?function=getAllUsers", {
+      headers: authHeader(),
+    });
+  }
+
+  getWhitelists() {
+    return instance.get("/get?function=getWhitelists", {
+      headers: authHeader(),
+    });
+  }
+
+  getCurrentUserInfo() {
+    return instance.get("/get?function=getUserInfo", {
+      headers: authHeader(),
+    });
+  }
+
+  getUserInfo(username: string) {
+    return instance.get("/get?function=getUserInfoOfUser&args=" + username, {
+      headers: authHeader(),
+    });
+  }
+}
+
+export default new UserManagementService();
