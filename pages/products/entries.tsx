859ecb4f846c73ef6fd98613d0433c85cc5358dc
Razvan Hrestic <razvanhrestic@INF8-MBHRESTIC.local>
2021-02-11 09:41:50 +0100

    BUGFIX: Build errors

--- pages/products/entries.tsx
+++ pages/products/entries.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import MainLayout from "../../src/components/layout/index";
 import GoodsEntry from "../../src/components/goods/goods-entry";
 
+
 export default function MetaInfoPage() {
   return (
     <MainLayout>
